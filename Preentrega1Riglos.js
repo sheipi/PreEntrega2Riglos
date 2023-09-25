cade78a527633dc69e008af9add31dc287d0cbb3
JuanPabloR <riglos.jp@gmail.com>
2023-09-25 13:29:24 -0300

    first

---
+++ Preentrega1Riglos.js
@@ -0,0 +1,17 @@
+let inver = parseInt(prompt("Ingrese un número: "));
+let tiempo = parseInt(prompt("Ingrese meses: "));
+let tasa = 1.18;
+let mes = 0;
+const resultadotna = (a, b, c) => a * b * c
+const resultadotea = (a, b) => a * b
+
+
+if (tiempo > 12) {
+    console.log("Lo siento. Solo brindamos información de un solo año")
+} else {
+    for (let i = 1; i <= tiempo; i++) {
+        mes = i;
+        console.log("resultado TNA mes", mes, " = ", resultadotna(inver, tasa, mes))
+    }
+    console.log("resultado TEA = ", resultadotea(resultadotna(inver, tasa, mes), 1.763))
+}
